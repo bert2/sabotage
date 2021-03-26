ee0505f7e70b2380b6adc3c8f438ab3b91cd37ec
bert2 <shuairan@gmail.com>
2021-03-26 14:18:17 +0100

    🔃 extract view model

---
+++ hyperactive/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace hyperactive {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using LibGit2Sharp;
+
+    public class MainViewModel: ViewModelBase {
+        private string directory = @"D:\DEV\git-conflicts";
+        public string Directory {
+            get => directory;
+            set => SetProperty(ref directory, value);
+        }
+
+        private Repository? repo;
+        public Repository? Repo {
+            get => repo;
+            set => SetProperty(ref repo, value, propertyName: null);
+        }
+
+        public IEnumerable<Branch>? Branches => repo?.Branches.Where(b => !b.IsRemote);
+    }
+}
--- hyperactive/MainWindow.xaml.cs
+++ hyperactive/MainWindow.xaml.cs
@@ -1,45 +1,18 @@
 ﻿namespace hyperactive {
     using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
     using System.IO;
-    using System.Linq;
     using System.Windows;
     using System.Windows.Forms;
 
     using LibGit2Sharp;
 
-    public partial class MainWindow : Window, INotifyPropertyChanged {
-        public MainWindow() => InitializeComponent();
+    public partial class MainWindow : Window {
+        private readonly MainViewModel mainViewModel = new();
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        private string directory = @"D:\DEV\git-conflicts";
-        public string Directory {
-            get => directory;
-            set {
-                if (value != directory) {
-                    directory = value;
-                    OnPropertyChanged(nameof(Directory));
-                }
-            }
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = mainViewModel;
         }
-
-        private Repository? repo;
-        private Repository? Repo {
-            get => repo;
-            set {
-                if (value != repo) {
-                    repo = value;
-                    OnPropertyChanged(nameof(Repo));
-                    OnPropertyChanged(nameof(Branches));
-                }
-            }
-        }
-
-        public IEnumerable<Branch>? Branches => repo?.Branches.Where(b => !b.IsRemote);
 
         private void OpenDirectory(object sender, RoutedEventArgs e) {
             using var dialog = new FolderBrowserDialog {
@@ -50,14 +23,14 @@
             };
 
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
-                Directory = dialog.SelectedPath;
+                mainViewModel.Directory = dialog.SelectedPath;
                 LoadDirectory(sender: null!, e: null!);
             }
         }
 
         private void LoadDirectory(object sender, RoutedEventArgs e) {
-            if (!Repository.IsValid(Directory)) return;
-            Repo = new Repository(Directory);
+            if (!Repository.IsValid(mainViewModel.Directory)) return;
+            mainViewModel.Repo = new Repository(mainViewModel.Directory);
         }
     }
 }
