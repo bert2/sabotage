50bf516f4b2e14f838c6d2f44c2f9a718532850d
bert2 <shuairan@gmail.com>
2021-03-27 13:55:16 +0100

    💄 sort directories first

--- hyperactive/BranchViewModel.cs
+++ hyperactive/BranchViewModel.cs
@@ -8,12 +8,21 @@
         private string name;
         public string Name { get => name; private set => SetProperty(ref name, value); }
 
-        private IEnumerable<TreeEntry> currentTree;
-        public IEnumerable<TreeEntry> CurrentTree { get => currentTree; private set => SetProperty(ref currentTree, value); }
+        private TreeEntry[] currentTree;
+        public TreeEntry[] CurrentTree { get => currentTree; private set => SetProperty(ref currentTree, value); }
 
         public BranchViewModel(Branch branch) {
             Name = branch.FriendlyName;
-            CurrentTree = branch.Tip.Tree;
+            CurrentTree = branch.Tip.Tree
+                .OrderBy(x => x, Comparer<TreeEntry>.Create(DirectoriesFirst))
+                .ToArray();
         }
+
+        private int DirectoriesFirst(TreeEntry a, TreeEntry b) => (a.Mode, b.Mode) switch {
+            (Mode.Directory, Mode.Directory) => a.Name.CompareTo(b.Name),
+            (Mode.Directory, _) => -1,
+            (_, Mode.Directory) => 1,
+            (_, _) => a.Name.CompareTo(b.Name)
+        };
     }
 }
