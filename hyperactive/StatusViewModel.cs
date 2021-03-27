dce09c30bf4e205d31338d6f5958509aae57d76f
bert2 <shuairan@gmail.com>
2021-03-27 12:36:39 +0100

    🔥 avoid superfluous property changed events

--- hyperactive/StatusViewModel.cs
+++ hyperactive/StatusViewModel.cs
@@ -3,43 +3,35 @@
 
     using MoreLinq;
 
-    public class StatusViewModel : ViewModel {
-        private int wdirAdd;
-        public int WdirAdd { get => wdirAdd; private set => SetProperty(ref wdirAdd, value); }
+    public class StatusViewModel {
+        public int WdirAdd { get; private set; }
 
-        private int wdirMod;
-        public int WdirMod { get => wdirMod; private set => SetProperty(ref wdirMod, value); }
+        public int WdirMod { get; private set; }
 
-        private int wdirDel;
-        public int WdirDel { get => wdirDel; private set => SetProperty(ref wdirDel, value); }
+        public int WdirDel { get; private set; }
 
-        private int wdirCon;
-        public int WdirCon { get => wdirCon; private set => SetProperty(ref wdirCon, value); }
+        public int WdirCon { get; private set; }
 
-        private int idxAdd;
-        public int IdxAdd { get => idxAdd; private set => SetProperty(ref idxAdd, value); }
+        public int IdxAdd { get; private set; }
 
-        private int idxMod;
-        public int IdxMod { get => idxMod; private set => SetProperty(ref idxMod, value); }
+        public int IdxMod { get; private set; }
 
-        private int idxDel;
-        public int IdxDel { get => idxDel; private set => SetProperty(ref idxDel, value); }
+        public int IdxDel { get; private set; }
 
-        private int idxCon;
-        public int IdxCon { get => idxCon; private set => SetProperty(ref idxCon, value); }
+        public int IdxCon { get; private set; }
 
         public StatusViewModel() { }
 
         public StatusViewModel(RepositoryStatus status) => status.ForEach(x => {
             switch (x.State) {
-                case FileStatus.NewInWorkdir:       wdirAdd++; break;
-                case FileStatus.ModifiedInWorkdir:  wdirMod++; break;
-                case FileStatus.DeletedFromWorkdir: wdirDel++; break;
-                case FileStatus.Conflicted:         wdirCon++; break;
+                case FileStatus.NewInWorkdir:       WdirAdd++; break;
+                case FileStatus.ModifiedInWorkdir:  WdirMod++; break;
+                case FileStatus.DeletedFromWorkdir: WdirDel++; break;
+                case FileStatus.Conflicted:         WdirCon++; break;
 
-                case FileStatus.NewInIndex:         idxAdd++; break;
-                case FileStatus.ModifiedInIndex:    idxMod++; break;
-                case FileStatus.DeletedFromIndex:   idxDel++; break;
+                case FileStatus.NewInIndex:         IdxAdd++; break;
+                case FileStatus.ModifiedInIndex:    IdxMod++; break;
+                case FileStatus.DeletedFromIndex:   IdxDel++; break;
 
                 case FileStatus.Ignored:
                 case FileStatus.Nonexistent:
