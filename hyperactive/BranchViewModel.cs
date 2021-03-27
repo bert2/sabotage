fba5755084ee30ab5e4fdaef8d777bef02f71510
bert2 <shuairan@gmail.com>
2021-03-27 13:48:08 +0100

    🚧 show tree of branch

--- hyperactive/BranchViewModel.cs
+++ hyperactive/BranchViewModel.cs
@@ -1,12 +1,19 @@
 ﻿namespace hyperactive {
+    using System.Collections.Generic;
+    using System.Linq;
+
     using LibGit2Sharp;
 
     public class BranchViewModel : ViewModel {
         private string name;
         public string Name { get => name; private set => SetProperty(ref name, value); }
 
+        private IEnumerable<TreeEntry> currentTree;
+        public IEnumerable<TreeEntry> CurrentTree { get => currentTree; private set => SetProperty(ref currentTree, value); }
+
         public BranchViewModel(Branch branch) {
             Name = branch.FriendlyName;
+            CurrentTree = branch.Tip.Tree;
         }
     }
 }
