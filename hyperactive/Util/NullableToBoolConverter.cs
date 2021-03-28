96b9fc4591bff8b267c30c3961e29f814ced86a7
bert2 <shuairan@gmail.com>
2021-03-28 16:47:48 +0200

    🐛 disable file editor when no file is selected

---
+++ hyperactive/Util/NullableToBoolConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace hyperactive {
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class NullableToBoolConverter : IValueConverter {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => value is not null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new InvalidOperationException($"{nameof(NullableToBoolConverter)} only works with OneWay bindings.");
+    }
+}
