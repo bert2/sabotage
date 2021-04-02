faef5bdf0e831a3b8f0c77c30a910ab02942c058
bert2 <shuairan@gmail.com>
2021-04-02 14:23:46 +0200

    ✨ validate against empty commit messages

---
+++ hyperactive/Util/ValidateNotEmpty.cs
@@ -0,0 +1,12 @@
+﻿namespace hyperactive {
+    using System.Globalization;
+    using System.Windows.Controls;
+
+    public class ValidateNotEmpty : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            return string.IsNullOrWhiteSpace(value?.ToString())
+                ? new ValidationResult(false, "cannot be empty")
+                : ValidationResult.ValidResult;
+        }
+    }
+}
--- hyperactive/ViewModels/EnterCommitMessage.cs
+++ hyperactive/ViewModels/EnterCommitMessage.cs
@@ -1,6 +1,15 @@
 ﻿namespace hyperactive {
     public class EnterCommitMessage : ViewModel {
         private string? commitMessage;
-        public string? CommitMessage { get => commitMessage; set => SetProperty(ref commitMessage, value); }
+        public string? CommitMessage {
+            get => commitMessage;
+            set {
+                if (SetProperty(ref commitMessage, value))
+                    Touched = true;
+            }
+        }
+
+        private bool touched;
+        public bool Touched { get => touched; set => SetProperty(ref touched, value); }
     }
 }
