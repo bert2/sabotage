da3997b86edd93bf7086812ebaea80ab203f47b4
bert2 <shuairan@gmail.com>
2021-04-02 17:32:09 +0200

    🐛 fix crash when leaving editor of branch that is not checked out

--- hyperactive/ViewModels/ObjDbFileContent.cs
+++ hyperactive/ViewModels/ObjDbFileContent.cs
@@ -1,13 +1,11 @@
 ﻿namespace hyperactive {
-    using System;
-
     using LibGit2Sharp;
 
     public class ObjDbFileContent: ViewModel, IFileContent {
         public bool ReadOnly { get; } = true;
 
         private readonly string? content;
-        public string? Content { get => content; set => throw new InvalidOperationException("Cannot edit git objects."); }
+        public string? Content { get => content; set { /*noop*/ } }
 
         public ObjDbFileContent(Blob blob) => content = blob.GetContentText();
     }
