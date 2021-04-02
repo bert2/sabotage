34c34767e04c4b593abdb4d5cfa8303a883532aa
bert2 <shuairan@gmail.com>
2021-04-02 14:41:56 +0200

    🚸 update file when leaving editor

--- hyperactive/MainWindow.xaml.cs
+++ hyperactive/MainWindow.xaml.cs
@@ -1,10 +1,16 @@
 ﻿namespace hyperactive {
     using System.Windows;
+    using System.Windows.Controls;
 
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
             DataContext = new Repo();
+        }
+
+        private void FileEditorLeft(object sender, RoutedEventArgs e) {
+            var editor = (TextBox)sender;
+            editor.GetBindingExpression(TextBox.TextProperty).UpdateSource();
         }
     }
 }
