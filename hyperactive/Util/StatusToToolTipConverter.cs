07d26e5600c139bdfeb06b2527b89b204ca6d1a2
bert2 <shuairan@gmail.com>
2021-04-05 16:28:00 +0200

    ✨ explain directory item coloring with tooltips

---
+++ hyperactive/Util/StatusToToolTipConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace hyperactive {
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(ItemStatus), typeof(string))]
+    public class StatusToToolTipConverter : IValueConverter {
+        public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+            => value is ItemStatus status && parameter is ItemType type
+                ? (type, status) switch {
+                    (_, ItemStatus.Unchanged)                => null,
+
+                    (ItemType.File, ItemStatus.Added)        => "added",
+                    (ItemType.File, ItemStatus.Modified)     => "modified",
+                    (ItemType.File, ItemStatus.Conflicted)   => "conflicted",
+                    (ItemType.File, ItemStatus.Ignored)      => "ignored",
+
+                    (ItemType.Folder, ItemStatus.Modified)   => "contents changed",
+                    (ItemType.Folder, ItemStatus.Conflicted) => "conflicts in content",
+                    _                                        => null
+                }
+                : null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new InvalidOperationException($"{nameof(StatusToToolTipConverter)} only works with OneWay bindings.");
+    }
+}
